93e20152d250e4ef7997ef29345dd87a6658318c
王旖 <1226007719@qq.com>
2020-02-13 13:06:23 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/android/gson/Weather.java
@@ -0,0 +1,16 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Weather {
+    public String status;
+    public Basic basic;
+    public Update update;
+    public Now now;
+    @SerializedName("lifestyle")
+    public List<Suggestion>suggestionList;
+    @SerializedName("daily_forecast")
+    public List<Forecast>forecastList;
+}
